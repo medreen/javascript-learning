9044f74807687932d098da81723ab3ae2b587f2d
medreen.wc <medreenwchege@gmail.com>
2025-11-15 17:44:52 +0300

    added changes

--- conditionaltask.js
+++ conditionaltask.js
@@ -2,34 +2,36 @@ let number = Number(prompt("Enter a number(check if even or odd): "))
 num1 = Number(prompt("Enter a number(check largest number): "))
 num2 = Number(prompt("Enter a number(check largest number): "))
 year = Number(prompt("Enter a year(check if leap year): "))
-temp = Number(prompt("Enter user temoerature(degrees celcius): "))
-x = Number(prompt("Enter a number(condition): "))
-y = Number(prompt("Enter a number(condition): "))
+temp = Number(prompt("Enter user temperature(degrees celcius): "))
+x = Number(prompt("Enter x: "))
+y = Number(prompt("Enter y: "))
 password = Number(prompt("Enter password(check password match): "))
 student_score = password = Number(prompt("Enter student score: "))
 attendance = password = Number(prompt("Enter student attendance: "))
 
 
 if (number%2 == 0){
-    console.log(`${number} is even`)
+    console.log(`${number} is even.`)
 } else {
-    console.log(`${number} is odd`)
+    console.log(`${number} is odd.`)
 }
 
 
 if (num1 > num2) {
-    console.log(`${num1} is greater than ${num2}`)
+    console.log(`${num1} is greater than ${num2}.`)
 } else if (num2 > num1) {
-    console.log(`${num2} is greater than ${num1}`)
+    console.log(`${num2} is greater than ${num1}.`)
 }
 
-
-if (String(year).endsWith("00") && year % 400){
-    console.log(`${year} is a leap year`)
-} else if(year % 4 && year % 100 != 0){
-    console.log(`${year} is a leap year`)
+if (year.length == 4) {
+    if (String(year).endsWith("00") && year % 400){
+        console.log(`${year} is a leap year.`)
+    } else if(year % 4 && year % 100 != 0){
+        console.log(`${year} is a leap year.`)
+    }
+} else {
+    console.log("Enter a valid year.")
 }
-
 
 if (temp > 30){
     console.log("The temperature is too high.")
@@ -40,15 +42,15 @@ if (temp > 30){
 }
 
 
-if (x >= 10 && x <= 20 || y > 100) {
-    console.log("Conditions met.")
+if (x >= 10 && x <= 20 && y > 100) {
+    console.log(`${x} and ${y}  meet condition.`)
 } else {
-    console.log("Conditions not met")
+    console.log(`${x} and ${y} don't meet condition.`)
 }
 
 
 secret_password = "secret123"
-if (password == secret_password) { console.log("Access") } else {console.log("Access denied")}
+if (password == secret_password) { console.log("Access granted.") } else {console.log("Access denied.")}
 
 
 if (student_score > 90){
