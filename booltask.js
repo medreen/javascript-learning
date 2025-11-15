ea8a2c96caf177c3b9112cc17877adebafdd9197
medreen.wc <medreenwchege@gmail.com>
2025-11-15 16:17:17 +0300

    added booltask

--- booltask.js
+++ booltask.js
@@ -1,10 +1,31 @@
 let number = Number(prompt("Enter a number: "))
+let email = prompt("Enter email: ")
+let phone = prompt("Enter your number: ")
 
-document.getElementById('demo').innerHTML = message
 
 if (number >= 0 && number <= 100){
-    message = ('Number is between 0 and 100.')
+    console.log('Number is between 0 and 100.')
 } else {
-    message = ('Number is not in range.')
+    console.log('Number is not in range.')
 }
+
+
+if (email.includes("@") && email.length <= 7 && email.includes(".com")){    
+    console.log("Email is valid.")}
+else{
+    console.log("Can't verify Email.")}
+
+
+if (phone.includes("+") && phone.startsWith("+") && phone.length == 13){    
+    console.log("Phone number is valid.")
+} else if(phone.length == 12 && phone.startsWith("254")){
+    console.log("Phone number is valid.")
+} else if (phone.length == 10 && phone.startsWith("0")){
+    console.log("Phone number is valid.")
+} else if(phone.length == 9 && phone.startsWith("1") || phone.startsWith("7")){
+    console.log("Phone number is valid.")
+} else {
+    console.log("Check number.")
+}
+
 
