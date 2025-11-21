0967cd74bc8c8c47d7004d844ed98c1406b09326
medreen.wc <medreenwchege@gmail.com>
2025-11-21 04:07:17 +0300

    added task

--- objecttask.js
+++ objecttask.js
@@ -1,15 +1,27 @@
 my_arr = [23, "Jane", 560, ["Lesson", "Maths", {"currency": "KES"}], 987, 76, "John"]
 
-//KES
+// //KES
 console.log(my_arr[3][2]["currency"])
 
-//560
+// //560
 console.log(my_arr[2])
 
-//Maths
+// //Maths
 console.log(my_arr[3][1])
 
+// //amount
 my_arr[3][2]["amount"] = 90
 console.log(my_arr)
 
-console.log(my_arr[4].sort())
+//reverse 987
+let num = String(my_arr[4]) 
+let x = num.split('')
+console.log(x) //['9', '8', '7']
+// x.reverse()
+// console.log(x) //['7', '8', '9']
+// let y = Number(x.join('') )
+// console.log(y)//789
+
+// //Jane to John
+my_arr[1] = "John"
+console.log(my_arr)
